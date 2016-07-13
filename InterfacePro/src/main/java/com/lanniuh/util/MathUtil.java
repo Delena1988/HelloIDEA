955ab989773e413cd9c3c97478c54e059bfb015b
Delena1988 <linjian19881026@gmail.com>
2016-07-13 20:59:48 +0800

    Initial commit

---
+++ InterfacePro/src/main/java/com/lanniuh/util/MathUtil.java
@@ -0,0 +1,11 @@
+package com.lanniuh.util;
+
+/**
+ * Created by linjian on 16/7/13.
+ */
+public class MathUtil {
+
+    public static int add(int a,int b){
+        return a+b;
+    }
+}
---
+++ InterfacePro/src/test/java/TestMathUtil.java
@@ -0,0 +1,13 @@
+import com.lanniuh.util.MathUtil;
+import org.junit.Test;
+
+/**
+ * Created by linjian on 16/7/13.
+ */
+public class TestMathUtil {
+    @Test
+    public void testAdd() {
+        int sum = MathUtil.add(1, 2);
+        System.out.println(sum);
+    }
+}
